e1d692a9b0f540e8efd131b6f8c9a66413a4b719
Milo <122271531+n4bru@users.noreply.github.com>
2026-02-13 09:41:49 +0100

    pokracovani

---
+++ main.py
@@ -0,0 +1,9 @@
+# Uvodni text by mel vypadat takto: 
+
+# Hi there!
+# -----------------------------------------------
+# I've generated a random 4 digit number for you.
+# Let's play a bulls and cows game.
+# -----------------------------------------------
+# Enter a number:
+# -----------------------------------------------
